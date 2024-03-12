fb2ced0cf86d76c8e00016a8a3d07bc68f4a0e44
Reid Aumann <141674197+reidaumann@users.noreply.github.com>
2024-03-12 18:45:53 +0000

    added bathrooms table and devise users

---
+++ app/views/bathrooms/_bathroom.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! bathroom, :id, :address, :hours, :accessible, :gender_neutral, :family_accessible, :purchase_required, :created_at, :updated_at
+json.url bathroom_url(bathroom, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
+  root "bathrooms#index"
+  devise_for :users
+  resources :bathrooms
   # root "articles#index"
 end
