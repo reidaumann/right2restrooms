931ff53946904097da152aff9df06fa0732bd209
Reid Aumann <141674197+reidaumann@users.noreply.github.com>
2024-04-24 17:00:38 +0000

    added pundit for users#show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,11 @@ class UsersController < ApplicationController
 
 def show
   @user = User.find(params[:id])
+  authorize @user
   @favorite_bathrooms = @user.favorite_bathrooms
-end
+
+  rescue Pundit::NotAuthorizedError
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to bathrooms_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root "bathrooms#landing_page"
 
   devise_for :users
-  get '/users/:id', to: 'users#show'
+  get 'users/:id', to: 'users#show', as: 'dynamic_user'
   
   resources :bathrooms
   resources :bathrooms do
